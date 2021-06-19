2592677ac73ec636c2f6f22fe2a5b7992c14dda2
Oreoluwa <Oreoluwa760@gmail.com>
2021-06-19 23:50:40 +0100

    Same Code for the DHT I'm currenlty soldeing

--- EcoBag.ino
+++ EcoBag.ino
@@ -1,65 +1,58 @@
-// Code For the Ecobag
-// Libraries for this Can be download on the google drive link http://bit.ly/librariesToFly_Ore
-// Written by Oluwafemi Oreoluwa December 25th 2020
-
-
 #include <LiquidCrystal.h>
-#include <Adafruit_Sensor.h>
+#include "DHT.h"
 #include <DHT.h>
 #include <DHT_U.h>
-#define DHTTYPE DHT22 
-#define DHTPIN 7  
-DHT_Unified dht(DHTPIN, DHTTYPE);
 
-LiquidCrystal seeme(1,2,3,4,5,6);
+#define DHTPIN 1
+#define DHTTYPE DHT11
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+DHT_Unified dht(DHTPIN, DHTTYPE);
 uint32_t delayMS;
 
-
 void setup() {
-  seeme.begin(16,2);
-  Serial.begin(9600);
-  dht.begin();
-  
-  Serial.println("Ecobag: Data Log");
-  sensor_t sensor;
-  dht.temperature().getSensor(&sensor);
-  
-  Serial.println("Temperature");
-  Serial.println(F("°C"));
-  Serial.println(F("°C"));
-  Serial.println(F("°C"));
+dht.begin();  
+lcd.begin(16, 2);
+lcd.setCursor(3,0);
+lcd.print("Epic Flow");
+delay(2000);
+lcd.clear();
+controllerpowerstate(); 
+sensor_t sensor;
+dht.temperature().getSensor(&sensor); 
+dht.humidity().getSensor(&sensor);
 
-
-  dht.humidity().getSensor(&sensor);
-  Serial.println("Humidity");
-  Serial.println(F("%"));
-  Serial.println(F("%"));
-  Serial.println(F("%"));
-  // Set delay between sensor readings based on sensor details.
-  delayMS = sensor.min_delay / 1000;
 }
 
 void loop() {
+sensors_event_t event;
+dht.temperature().getEvent(&event);
+lcd.setCursor(1,0);
+lcd.print(F("Temp: "));
+lcd.setCursor(8,0);
+lcd.print(event.temperature);
+
+
   
-  delay(delayMS);
-  sensors_event_t event;            // Get temperature event and print its value.
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
-    Serial.println(F("Error - No Temperature Reading"));
-  }
-  else {
-    Serial.print(F("Temperature: "));
-    Serial.print(event.temperature);
-    Serial.println(F("°C"));
-  }
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println(F("Error reading humidity!"));
-  }
-  else {
-    Serial.print(F("Humidity: "));
-    Serial.print(event.relative_humidity);
-    Serial.println(F("%"));
+}
+
+
+ 
+int controllerpowerstate(){
+  pinMode(9,OUTPUT);
+  digitalWrite(9,HIGH);
+  delay(1000);
+  digitalWrite(9,LOW);
+  delay(1000);
+}
+
+int chargingalert(){
+  while(1){
+  lcd.setCursor(6,0);
+  lcd.print("Low Power");
+  lcd.clear();
+  lcd.setCursor(6,0);
+  lcd.print("Low Power");
   }
 }
